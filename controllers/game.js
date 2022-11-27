4ccdb6178c6ebbb7f5441f9a063e5d91c822839b
Mete <113627974+MeteKat@users.noreply.github.com>
2022-11-27 06:10:07 +0300

    v2 - Game functions populated, database populated

--- controllers/game.js
+++ controllers/game.js
@@ -1,28 +1,53 @@
 const Card = require('../models/card')
 
-let GameParameters = {
+GameParameters = {
     Turn: String,
-    OnGoing: Boolean
+    Started: Boolean,
 }
-// const card = await Card.create({user: userName})
 
-const drawCard = async (userName,) => {
+UserParameters = {
+    FirstUser: {
+        DrewCard:false,
+    },
+    SecondUser: {
+        DrewCard:false,
+    }
+}
+
+
+const drawCard = async (userName) => {
     // Check if player already drew a card this turn
+    const user =await Card.findOne({name:userName})
+    if(user.drewCard){
+        throw new Error({message:"You cannot draw more than once in a turn"})
+    }
+
     // Create a random card
+    const randomCard=createRandomCard()
+
     // Update player's hand on database
-    // Return both players hand
+    await user.cardDraw({randomCard})
+
+    // Return both players hand ???????????
 }
 
-                                    //region TODOS
-                                    // TODO - Inıt cards
-                                    // TODO - Start game
-                                    // TODO - Remove from route
-                                    //endregion
-
-const startGame = async () => {
+const startGame = async (userName1,userName2) => {
     // Create 5 random cards for each player
+    const userCards1 = createRandomCard(5)
+    const userCards2 = createRandomCard(5)
+
     // Initialize players hand and write to database
+    await Card.create({user:{name:userName1.toString(),cards:userCards1}})
+    await Card.create({user:{name:userName2.toString(),cards:userCards2}})
+
     // Put a random card to the board
+    const starterCard = createRandomCard()
+
+    // Set OnGoing true
+    GameParameters.Started=true
+
+    // Return player hands and starter card
+    return {starterCard,userCards1,userCards2}
 }
 
 const makeMove = async (userName, card) => {
@@ -33,8 +58,40 @@ const makeMove = async (userName, card) => {
 
 const isGameOver = () => {
     // Check if any of the players hand is empty after a move
-    // Set Game.
+    // Set OnGoing false
+    // Redirect to some page
+    // Save/Remove previous game cards
 }
 
+
+const createRandomCard = (count=1) => {
+    let cardStack = []
+    for (let i = 0; i < count; i++) {
+        let randomNumber = Math.floor(Math.random() * 112)
+        let color;
+        let text;
+
+        if (randomNumber < 25) {
+            color = "Red"
+        } else if (randomNumber < 50) {
+            color = "Yellow"
+        } else if (randomNumber < 75) {
+            color = "Green"
+        } else if (randomNumber < 100) {
+            color = "Blue"
+        } else {
+            color = "Black"
+        }
+        if (color !== "Black") {
+            text = Math.floor(Math.random() * 10).toString()
+        } else {
+            const textValues = ["+2", "+4", "Wild"]
+            text = textValues[Math.floor(Math.random() * 3)]
+        }
+        cardStack.push({text, color})
+    }
+
+    return cardStack
+}
 
 module.exports = {drawCard, startGame, makeMove, isGameOver}
--- models/card.js
+++ models/card.js
@@ -2,46 +2,31 @@ const mongoose = require('mongoose')
 
 
 const CardSchema = new mongoose.Schema({
-    color: {
-        type: String,
-        enum: ["Red", "Yellow", "Blue", "Green", "Black"],
-        required: false
-    },
-    text: {
-        type: String,
-        required: false
-    },
+
     user: {
-        type: String,
-        required:true
+        name: String,
+        cards: {
+            type: Array,
+            color: {
+                type: String,
+                enum: ["Red", "Yellow", "Blue", "Green", "Black"],
+            },
+            text: {
+                type: String,
+            }
+        },
+        drewCard: {
+            type: Boolean,
+            default: false
+        }
+
     }
 })
 
-/// Random card generate
-CardSchema.pre('save', function () {
-    let randomNumber = Math.floor(Math.random() * 112)
-    let pickedCardColor;
-    let pickedCardText;
+CardSchema.methods.cardDraw = function (card) {
+    this.cards.unshift({card})
+    this.drewCard=true       // TODO - Add card to players hand
+}
 
-    if (randomNumber < 25) {
-        pickedCardColor = "Red"
-    } else if (randomNumber < 50) {
-        pickedCardColor = "Yellow"
-    } else if (randomNumber < 75) {
-        pickedCardColor = "Green"
-    } else if (randomNumber < 100) {
-        pickedCardColor = "Blue"
-    } else {
-        pickedCardColor = "Black"
-    }
-    if (pickedCardColor !== "Black") {
-        pickedCardText = Math.floor(Math.random() * 10).toString()
-    } else {
-        const textValues = ["+2", "+4", "Wild"]
-        pickedCardText = textValues[Math.floor(Math.random() * 3)]
-    }
-    this.color = pickedCardColor
-    this.text = pickedCardText
-})
 
 module.exports = mongoose.model('Card', CardSchema)
